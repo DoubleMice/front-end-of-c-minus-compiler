4caa7107f6cc17a412e01b44a946da45a23461af
foxnuaaer <foxnuaaer@nuaa.edu.cn>
2017-12-12 08:15:06 +0800

    finish lexer

---
+++ src/token.h
@@ -0,0 +1,27 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+/* +  -  *  /  <  <=  >  >=  ==  !=  =  ;  ,  (  )  [  ]  {  } */
+/* \/\*  \*\/ */
+/* if  else  return  void  while */
+/* relop(to deal with signal) */
+/* int(to deal with number) */
+typedef enum {
+	PLUS = 256,MINUS,MULTI,DEVIDE,LT,LE,GT,GE,SAME,NE,EQ,SEMI,COMMA,LB,RB,LSB,RSB,LCB,RCB,
+	LN,RN,
+	IF,ELSE,RETURN,VOID,WHILE,
+	ID,NUM,
+	RELOP,
+    INT
+} TType;
+
+static void print_token(int token) {
+    static char *token_strs[] = {
+        "PLUS","MINUS","MULTI","DEVIDE","LT","LE","GT","GE","SAME","NE","EQ","SEMI","COMMA","LB","RB","LSB","RSB",
+        "LCB","RCB","LN","RN","IF","ELSE","RETURN","VOID","WHILE","ID","NUM","RELOP","INT"
+    };
+    if (token < 255)
+        printf("%-20c", token);
+    else
+        printf("%-20s",token_strs[token-256]);
+}
+#endif
