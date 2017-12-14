e82c62a8b0ed5314eaa4affaef6bea6876142822
foxnuaaer <foxnuaaer@nuaa.edu.cn>
2017-12-14 11:23:57 +0800

    ADD(symbol table && hash.h)

--- src/symbol.h
+++ src/symbol.h
@@ -1,6 +1,40 @@
 #ifndef SYMBOL_H
 #define SYMBOL_H
+#include "hash.h"
+#define uint unsigned int
+#define uint64 unsigned long int 
 
+typedef enum {
+    LOCAL,GLOBAL
+} STBind;
 
+typedef enum {
+    DEFAULT,
+} STVis;
+
+typedef struct {
+    uint st_Val;
+    uint st_Num;
+    char *st_Bind;
+    CM_Sym *Next;
+    char st_name[16];
+}CM_Sym;
+
+#define OFFSET sizeof(CM_Sym)
+
+static void ST_add(CM_Sym *symbol,char *name) {
+	CM_Sym *st = symbol;
+	uint hash = time33(name);
+	while(st->st_Val != hash)
+		st = st->Next;
+	if(st->Next == 0) {
+		st->Next = st + 1;
+		st = st->Next;
+		st->Next->st_Val = hash;
+		st->Next->st_Num = st->st_Val + 1;
+		st->Next->st_Bind;
+		st->Next->st_name;
+	}
+}
 
 #endif
--- src/token.h
+++ src/token.h
@@ -9,15 +9,21 @@ typedef enum {
 	PLUS = 256,MINUS,MULTI,DEVIDE,LT,LE,GT,GE,SAME,NE,EQ,SEMI,COMMA,LB,RB,LSB,RSB,LCB,RCB,
 	LN,RN,
 	IF,ELSE,RETURN,VOID,WHILE,
-	ID,NUM,
+	ID,NUM,FUNC
 	RELOP,
     INT
 } TType;
 
+typedef struct {
+    int TT_type;
+    Token_Table *Next;
+    char TT_value[16];
+} Token_Table;
+
 static void print_token(int token) {
     static char *token_strs[] = {
         "PLUS","MINUS","MULTI","DEVIDE","LT","LE","GT","GE","SAME","NE","EQ","SEMI","COMMA","LB","RB","LSB","RSB",
-        "LCB","RCB","LN","RN","IF","ELSE","RETURN","VOID","WHILE","ID","NUM","RELOP","INT"
+        "LCB","RCB","LN","RN","IF","ELSE","RETURN","VOID","WHILE","ID","NUM","FUNC","RELOP","INT"
     };
     if (token < 255)
         printf("%-20c", token);
